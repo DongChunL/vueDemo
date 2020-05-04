be6629b109c72f29dd22fee8c57abf176aa8284f
email@qq.com <1368706321@qq.com>
2020-05-04 10:09:25 +0800

    commit staticComment

---
+++ src-staticComment/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <header class="site-header jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1>请发表对Vue的评论</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <Add/>
+            <List :comments="comments"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Add from './components/Add.vue'
+    import List from './components/List.vue'
+
+    export default {
+        name: "App",
+        components:{
+            Add,
+            List
+        },
+        data(){
+            return {
+                comments: [
+                    {
+                        name:'BOB',
+                        content:'Vue你好'
+                    },
+                    {
+                        name:'丁淑玉',
+                        content:'Vue你好哈'
+                    },
+                    {
+                        name:'凯文',
+                        content:'Vue你好哦'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .reply {
+        margin-top: 0px;
+    }
+</style>
