e7ac7a045cd8b20f1b538341e72c8eceed70fda0
email@qq.com <1368706321@qq.com>
2020-05-05 19:35:38 +0800

    计划表升级版，JSON+localStorage实现数据本地缓存和读取

--- src-planTablePlus/App.vue
+++ src-planTablePlus/App.vue
@@ -17,17 +17,19 @@
     export default {
         data() {
             return {
-                todos: [
+                //1、从localstorage读取todos
+                /*
+                * window.localStorage.getItem(K-V),获得本地存储数据，返回文本格式的内容
+                * JSON.parse("String")将文本内容转化为JSON格式
+                * */
+                todos: JSON.parse(window.localStorage.getItem('todos_key') || '[]')
+                //2、深度监视，监视任何改动；一般监视，只监视外部
+                /*[
                     {title: "吃饭",complete:false},
                     {title: "睡觉",complete: true},
                     {title: "coding",complete: false}
-                ]
+                ]*/
             }
-        },
-        components:{
-            TodoHeader,
-            TodoList,
-            TodoFooter
         },
         methods: {
             addPlan(todo){
@@ -40,17 +42,33 @@
 
             //清除选中的，不改变值，  不用参数
             deleteCompleteTodos(){
-            /*    this.todos = [''];*/
+                /*    this.todos = [''];*/
                 this.todos = this.todos.filter(todo=>!todo.complete)
             },
             //更改complete值为true或false，故传参。参数checked即代表全选框是否选中（true,false）
             selectAllTodos(checked){
                 this.todos.forEach(todo=>todo.complete=checked);
-            /*    for(let i=0;i<this.todos.length;i++){
-                    todos[i].complete=true;
-                }*/
+                /*    for(let i=0;i<this.todos.length;i++){
+                        todos[i].complete=true;
+                    }*/
             }
-        }
+        },
+        watch:{//深度监视
+            todos:{
+                deep: true, //监听的数据深度，一般用来监听对象中某个属性的变化
+                //handle是深度监视watch中需要具体执行的方法，类型是回调函数，配置handler: function(new,old)
+                handler: function(newValue){
+                    //将todos最新的值，转换成JSON格式的字符串，保存到localStorage
+                    window.localStorage.setItem('todos_key',JSON.stringify(newValue))
+                }
+            }
+        },
+        components:{
+            TodoHeader,
+            TodoList,
+            TodoFooter
+        },
+
     }
 </script>
 
